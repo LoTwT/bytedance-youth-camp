f64a7d98e2d43da49c160197293fd84a2632ee12
LoTwT <709937065@qq.com>
2021-09-20 14:06:16 +0800

    feat(day_01): cli init finish

--- src/day_01/vue-auto-router-cli/lib/init.js
+++ src/day_01/vue-auto-router-cli/lib/init.js
@@ -4,6 +4,22 @@ const figlet = promisify(require("figlet"))
 const clear = require("clear")
 const chalk = require("chalk")
 const { clone } = require("./download")
+const open = require("open")
+
+// 需要解决兼容性问题
+// 实现一个 promise 风格的 spawn()
+const spawn = async (...args) => {
+  const { spawn } = require("child_process")
+
+  return new Promise((resolve) => {
+    // 子进程输出流 合并到 主进程
+    const proc = spawn(...args)
+    proc.stdout.pipe(process.stdout)
+    proc.stderr.pipe(process.stderr)
+
+    proc.on("close", () => resolve())
+  })
+}
 
 const log = (content) => console.log(chalk.green(content))
 
@@ -14,6 +30,14 @@ module.exports = async (name) => {
   log(data)
 
   // 项目模板
-  log("创建项目" + name)
-  await clone("github:su37josephxia/vue-template", name)
+  log("create template" + name)
+  // await clone("github:su37josephxia/vue-template", name)
+
+  // 用子进程下载依赖
+  log(`install dependencies...`)
+  await spawn("npm", ["install"], { cwd: `./${name}`, shell: true })
+
+  log("finish...")
+  open("http://localhost: 8080")
+  await spawn("npm", ["run", "serve"], { cwd: `./${name}`, shell: true })
 }
